2635869f000699ad25c72e6d842e4f378744b078
QuentinN <quentinnicolet@gmail.com>
2023-02-28 16:58:28 +0100

    First Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <h1>Kasa</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
